349c164da97079c1adc3724bdc5a837f3ed6ff9b
dan5 <dan5.ya@gmail.com>
2011-08-10 07:02:33 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jewelmmo::Application.config.secret_token = 'a94b14928f924742c7691f1d0ef6da6cf679b5b139e39e1d92f749f5f49791c9a1b0771d1d5a5c01ff83c83a2e70ceda6f5de2856af38625652c62a29c4d18db'
