01aa460a62da20568b87246fff51b2d9ecbede08
dan5 <dan5.ya@gmail.com>
2011-09-06 07:12:55 +0900

    spec追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110831170725) do
+ActiveRecord::Schema.define(:version => 20110824060418) do
 
   create_table "dolls", :force => true do |t|
     t.integer  "user_id"
