0de6ff203c2f88dce4f1771110550bebb5c0af93
dan5 <dan5.ya@gmail.com>
2011-09-06 16:34:39 +0900

    dollのspecを追加

--- spec/models/doll_spec.rb
+++ spec/models/doll_spec.rb
@@ -1,5 +1,38 @@
+# -*- encoding: utf-8 -*-
 require 'spec_helper'
 
 describe Doll do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user = User.create
+    3.times do
+      free_doll = FreeDoll.create
+      user.choice free_doll.id
+    end
+    @dolls = user.dolls
+    @doll = @dolls.first
+  end
+
+  it "まずdollはリーダーである" do
+    @doll.leader?.should be_true
+  end
+
+  describe Doll do
+    before do
+      @doll.follow @dolls[1]
+    end
+
+    it "dollはリーダーではない" do
+      @doll.leader?.should be_false
+    end
+  end
+
+  describe Doll do
+    before do
+      @dolls[1].follow @doll
+    end
+
+    it "dollはリーダーである" do
+      @doll.leader?.should be_true
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
+# -*- encoding: utf-8 -*-
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create
+  end
+
+  it { @user.dolls.should be_empty }
+
+  describe "free dollを選んでdollを獲得" do
+    before do
+      free_doll = FreeDoll.create
+      @user.choice free_doll.id
+    end
+
+    it { FreeDoll.count.should == 0 }
+    it { @user.dolls.size.should == 1 }
+  end
 end
